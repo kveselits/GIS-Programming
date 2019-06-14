3596fab25aeea3ae16781aee7ccbcff331bf6861
Kris Veselits <43455376+kveselits@users.noreply.github.com>
2019-06-14 00:07:45 -0700

    Update BrokenLink.py

--- BrokenLink.py
+++ BrokenLink.py
@@ -4,6 +4,7 @@ mxd = arcpy.mapping.MapDocument("N:\\GIS Programming\\Activity3.mxd")
 print mxd.filePath
 layerlist = arcpy.mapping.ListLayers(mxd)
 
+##count the number of broken links within the layer list:
 numBrokenLinks = 0
 for lyr in layerlist:
     if lyr.isBroken:
